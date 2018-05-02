bbb18213de72b9b7ddb9252295e3e80e45dd4483
Sam Greydanus <samjgrey3@gmail.com>
2018-05-02 09:29:13 -0700

    major code refactor
    * use GRU instead of LSTM
    * support PyTorch 0.4
    * use Python 3.6
    
    Life is good

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ Sam Greydanus | October 2017 | MIT License
 
 Results after training on 40M frames:
 
-![breakout-v0.gif](breakout-v0/breakout-v0.gif)
-![pong-v0.gif](pong-v0/pong-v0.gif)
-![spaceinvaders-v0.gif](spaceinvaders-v0/spaceinvaders-v0.gif)
+![breakout-v0.gif](breakout-v4/breakout-v4.gif)
+![pong-v0.gif](pong-v4/pong-v4.gif)
+![spaceinvaders-v0.gif](spaceinvaders-v4/spaceinvaders-v4.gif)
 
 Usage
 --------
 
-If you're working on OpenAI's [Breakout-v0](https://gym.openai.com/envs/Breakout-v0/) environment:
- * To train: `python baby-a3c.py --env Breakout-v0`
- * To test: `python baby-a3c.py --env Breakout-v0 --test True`
- * To render: `python baby-a3c.py --env Breakout-v0 --render True`
+If you're working on OpenAI's [Breakout-v4](https://gym.openai.com/envs/Breakout-v4/) environment:
+ * To train: `python baby-a3c.py --env Breakout-v4`
+ * To test: `python baby-a3c.py --env Breakout-v4 --test True`
+ * To render: `python baby-a3c.py --env Breakout-v4 --render True`
 
 About
 --------
@@ -29,24 +29,37 @@ Frustrated by the number of deep RL implementations that are clunky and opaque? 
  * If you want to **try something new**, this is a simple and strong baseline
  * Here's a [quick intro to A3C](https://goo.gl/Ub3vCY) that I wrote
 
-|			                         | Breakout-v0  | Pong-v0       | SpaceInvaders-v0  |
+|			                         | Breakout-v4  | Pong-v4       | SpaceInvaders-v4  |
 | -------------                      |:------------:| :------------:| :------------:    |
-| *Mean episode rewards @ 40M frames | 290 ± 10     | 20.2 ± 0.1    |   425 ± 10        |
-| *Mean episode rewards @ 80M frames | 320 ± 10     | 20.2 ± 0.1    |   425 ± 10        |
+| *Mean episode rewards @ 40M frames | 140 ± 20     | 18.2 ± 1    |   470 ± 30        |
+| *Mean episode rewards @ 80M frames | 190 ± 20     | 17.9 ± 1    |   550 ± 30        |
 
 \*same (default) hyperparameters across all environments
+
+Architecture
+--------
+
+```python
+self.conv1 = nn.Conv2d(channels, 32, 3, stride=2, padding=1)
+self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+self.gru = nn.GRUCell(32 * 5 * 5, memsize) # *see below
+self.critic_linear, self.actor_linear = nn.Linear(memsize, 1), nn.Linear(memsize, num_actions)
+```
+
+\*we use a GRU cell because it has fewer params, uses one memory vector instead of two, and attains the same performance as an LSTM cell.
 
 Environments that work
 --------
 _(Use `pip freeze` to check your environment settings)_
- * Mac OSX or Linux
- * Python 2.7 or 3.6
- * NumPy 1.13.1
- * Gym 0.9.4
+ * Mac OSX (test mode only) or Linux (train and test)
+ * Python 3.6
+ * NumPy 1.13.1+
+ * Gym 0.9.4+
  * SciPy 0.19.1 (just on two lines -> workarounds possible)
- * [PyTorch 0.2.0](http://pytorch.org/)
+ * [PyTorch 0.4.0](http://pytorch.org/)
 
 Known issues
 --------
- * Python 2.7 + PyTorch 0.2.0 + Mac OSX produces a **segfault**
-   * workaround: revert to PyTorch 0.1.12 (`pip2 install http://download.pytorch.org/whl/torch-0.1.12.post2-cp27-none-macosx_10_7_x86_64.whl`)
+ * I recently ported this code to Python 3.6 / PyTorch 0.4. If you want to run on Python 2.7 / PyTorch 0.2, then look at one of my earlier commits to this repo (there are different pretrained models as well)
--- baby-a3c.py
+++ baby-a3c.py
@@ -1,58 +1,54 @@
 # Baby Advantage Actor-Critic | Sam Greydanus | October 2017 | MIT License
 
 from __future__ import print_function
-import torch, os, gym, time, glob, argparse
+import torch, os, gym, time, glob, argparse, sys
 import numpy as np
 from scipy.signal import lfilter
 from scipy.misc import imresize # preserves single-pixel info _unlike_ img = img[::2,::2]
-
 import torch.nn as nn
-from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.multiprocessing as mp
 os.environ['OMP_NUM_THREADS'] = '1'
 
-parser = argparse.ArgumentParser(description=None)
-parser.add_argument('--env', default='Breakout-v0', type=str, help='gym environment')
-parser.add_argument('--processes', default=20, type=int, help='number of processes to train with')
-parser.add_argument('--render', default=False, type=bool, help='renders the atari environment')
-parser.add_argument('--test', default=False, type=bool, help='test mode sets lr=0, chooses most likely actions')
-parser.add_argument('--lstm_steps', default=20, type=int, help='steps to train LSTM over')
-parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')
-parser.add_argument('--seed', default=1, type=int, help='seed random # generators (for reproducibility)')
-parser.add_argument('--gamma', default=0.99, type=float, help='discount for gamma-discounted rewards')
-parser.add_argument('--tau', default=1.0, type=float, help='discount for generalized advantage estimation')
-parser.add_argument('--horizon', default=0.99, type=float, help='horizon for running averages')
-args = parser.parse_args()
-
-args.save_dir = '{}/'.format(args.env.lower()) # keep the directory structure simple
-if args.render:  args.processes = 1 ; args.test = True # render mode -> test mode w one process
-if args.test:  args.lr = 0 # don't train in render mode
-args.num_actions = gym.make(args.env).action_space.n # get the action space of this game
-os.makedirs(args.save_dir) if not os.path.exists(args.save_dir) else None # make dir to save models etc.
+def get_args():
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('--env', default='Breakout-v4', type=str, help='gym environment')
+    parser.add_argument('--processes', default=20, type=int, help='number of processes to train with')
+    parser.add_argument('--render', default=False, type=bool, help='renders the atari environment')
+    parser.add_argument('--test', default=False, type=bool, help='sets lr=0, chooses most likely actions')
+    parser.add_argument('--rnn_steps', default=20, type=int, help='steps to train LSTM over')
+    parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')
+    parser.add_argument('--seed', default=1, type=int, help='seed random # generators (for reproducibility)')
+    parser.add_argument('--gamma', default=0.99, type=float, help='rewards discount factor')
+    parser.add_argument('--tau', default=1.0, type=float, help='generalized advantage estimation discount')
+    parser.add_argument('--horizon', default=0.99, type=float, help='horizon for running averages')
+    parser.add_argument('--hidden', default=256, type=int, help='hidden size of GRU')
+    return parser.parse_args()
 
 discount = lambda x, gamma: lfilter([1],[1,-gamma],x[::-1])[::-1] # discounted rewards one liner
 prepro = lambda img: imresize(img[35:195].mean(2), (80,80)).astype(np.float32).reshape(1,80,80)/255.
 
 def printlog(args, s, end='\n', mode='a'):
     print(s, end=end) ; f=open(args.save_dir+'log.txt',mode) ; f.write(s+'\n') ; f.close()
-        
-class NNPolicy(torch.nn.Module): # an actor-critic neural network
-    def __init__(self, channels, num_actions):
+
+class NNPolicy(nn.Module): # an actor-critic neural network
+    def __init__(self, channels, memsize, num_actions):
         super(NNPolicy, self).__init__()
         self.conv1 = nn.Conv2d(channels, 32, 3, stride=2, padding=1)
         self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
         self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
         self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-        self.lstm = nn.LSTMCell(32 * 5 * 5, 256)
-        self.critic_linear, self.actor_linear = nn.Linear(256, 1), nn.Linear(256, num_actions)
+        self.gru = nn.GRUCell(32 * 5 * 5, memsize)
+        self.critic_linear, self.actor_linear = nn.Linear(memsize, 1), nn.Linear(memsize, num_actions)
 
-    def forward(self, inputs):
-        inputs, (hx, cx) = inputs
-        x = F.elu(self.conv1(inputs)) ; x = F.elu(self.conv2(x))
-        x = F.elu(self.conv3(x)) ; x = F.elu(self.conv4(x))
-        hx, cx = self.lstm(x.view(-1, 32 * 5 * 5), (hx, cx))
-        return self.critic_linear(hx), self.actor_linear(hx), (hx, cx)
+    def forward(self, inputs, train=True, hard=False):
+        inputs, hx = inputs
+        x = F.elu(self.conv1(inputs))
+        x = F.elu(self.conv2(x))
+        x = F.elu(self.conv3(x))
+        x = F.elu(self.conv4(x))
+        hx = self.gru(x.view(-1, 32 * 5 * 5), (hx))
+        return self.critic_linear(hx), self.actor_linear(hx), hx
 
     def try_load(self, save_dir):
         paths = glob.glob(save_dir + '*.tar') ; step = 0
@@ -78,22 +74,32 @@ class SharedAdam(torch.optim.Adam): # extend a pytorch optimizer so it shares gr
                 for p in group['params']:
                     if p.grad is None: continue
                     self.state[p]['shared_steps'] += 1
-                    self.state[p]['step'] = self.state[p]['shared_steps'][0] - 1 # there's a "step += 1" later
+                    self.state[p]['step'] = self.state[p]['shared_steps'][0] - 1 # a "step += 1"  comes later
             super.step(closure)
 
-torch.manual_seed(args.seed)
-shared_model = NNPolicy(channels=1, num_actions=args.num_actions).share_memory()
-shared_optimizer = SharedAdam(shared_model.parameters(), lr=args.lr)
+def cost_func(args, values, logps, actions, rewards):
+    np_values = values.view(-1).data.numpy()
 
-info = {k : torch.DoubleTensor([0]).share_memory_() for k in ['run_epr', 'run_loss', 'episodes', 'frames']}
-info['frames'] += shared_model.try_load(args.save_dir)*1e6
-if int(info['frames'][0]) == 0: printlog(args,'', end='', mode='w') # clear log file
+    # generalized advantage estimation using \delta_t residuals (a policy gradient method)
+    delta_t = np.asarray(rewards) + args.gamma * np_values[1:] - np_values[:-1]
+    logpys = logps.gather(1, torch.tensor(actions).view(-1,1))
+    gen_adv_est = discount(delta_t, args.gamma * args.tau)
+    policy_loss = -(logpys.view(-1) * torch.FloatTensor(gen_adv_est.copy())).sum()
+    
+    # l2 loss over value estimator
+    rewards[-1] += args.gamma * np_values[-1]
+    discounted_r = discount(np.asarray(rewards), args.gamma)
+    discounted_r = torch.tensor(discounted_r.copy(), dtype=torch.float32)
+    value_loss = .5 * (discounted_r - values[:-1,0]).pow(2).sum()
 
-def train(rank, args, info):
+    entropy_loss = -(-logps * torch.exp(logps)).sum() # encourage lower entropy
+    return policy_loss + 0.5 * value_loss + 0.01 * entropy_loss
+
+def train(shared_model, shared_optimizer, rank, args, info):
     env = gym.make(args.env) # make a local (unshared) environment
     env.seed(args.seed + rank) ; torch.manual_seed(args.seed + rank) # seed everything
-    model = NNPolicy(channels=1, num_actions=args.num_actions) # init a local (unshared) model
-    state = torch.Tensor(prepro(env.reset())) # get first state
+    model = NNPolicy(channels=1, memsize=args.hidden, num_actions=args.num_actions) # a local/unshared model
+    state = torch.tensor(prepro(env.reset())) # get first state
 
     start_time = last_disp_time = time.time()
     episode_length, epr, eploss, done  = 0, 0, 0, True # bookkeeping
@@ -101,78 +107,81 @@ def train(rank, args, info):
     while info['frames'][0] <= 8e7 or args.test: # openai baselines uses 40M frames...we'll use 80M
         model.load_state_dict(shared_model.state_dict()) # sync with shared model
 
-        cx = Variable(torch.zeros(1, 256)) if done else Variable(cx.data) # lstm memory vector
-        hx = Variable(torch.zeros(1, 256)) if done else Variable(hx.data) # lstm activation vector
+        hx = torch.zeros(1, 256) if done else hx.detach()  # rnn activation vector
         values, logps, actions, rewards = [], [], [], [] # save values for computing gradientss
 
-        for step in range(args.lstm_steps):
+        for step in range(args.rnn_steps):
             episode_length += 1
-            value, logit, (hx, cx) = model((Variable(state.view(1,1,80,80)), (hx, cx)))
-            logp = F.log_softmax(logit)
+            value, logit, hx = model((state.view(1,1,80,80), hx))
+            logp = F.log_softmax(logit, dim=-1)
 
-            action = logp.max(1)[1].data if args.test else torch.exp(logp).multinomial().data[0]
+            action = torch.exp(logp).multinomial(num_samples=1).data[0]#logp.max(1)[1].data if args.test else
             state, reward, done, _ = env.step(action.numpy()[0])
             if args.render: env.render()
 
-            state = torch.Tensor(prepro(state)) ; epr += reward
+            state = torch.tensor(prepro(state)) ; epr += reward
             reward = np.clip(reward, -1, 1) # reward
-            done = done or episode_length >= 1e4 # keep agent from playing one episode too long
+            done = done or episode_length >= 1e4 # don't playing one ep for too long
             
-            info['frames'] += 1 ; num_frames = int(info['frames'][0])
+            info['frames'].add_(1) ; num_frames = int(info['frames'].item())
             if num_frames % 2e6 == 0: # save every 2M frames
                 printlog(args, '\n\t{:.0f}M frames: saved model\n'.format(num_frames/1e6))
                 torch.save(shared_model.state_dict(), args.save_dir+'model.{:.0f}.tar'.format(num_frames/1e6))
 
-            if done: # update shared data. maybe print info.
+            if done: # update shared data
                 info['episodes'] += 1
                 interp = 1 if info['episodes'][0] == 1 else 1 - args.horizon
                 info['run_epr'].mul_(1-interp).add_(interp * epr)
                 info['run_loss'].mul_(1-interp).add_(interp * eploss)
 
-                if rank ==0 and time.time() - last_disp_time > 60: # print info ~ every minute
-                    elapsed = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - start_time))
-                    printlog(args, 'time {}, episodes {:.0f}, frames {:.1f}M, run epr {:.2f}, run loss {:.2f}'
-                        .format(elapsed, info['episodes'][0], num_frames/1e6, info['run_epr'][0], info['run_loss'][0]))
-                    last_disp_time = time.time()
+            if rank == 0 and time.time() - last_disp_time > 60: # print info ~ every minute
+                elapsed = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - start_time))
+                printlog(args, 'time {}, episodes {:.0f}, frames {:.1f}M, mean epr {:.2f}, run loss {:.2f}'
+                    .format(elapsed, info['episodes'].item(), num_frames/1e6,
+                    info['run_epr'].item(), info['run_loss'].item()))
+                last_disp_time = time.time()
 
+            if done: # maybe print info.
                 episode_length, epr, eploss = 0, 0, 0
-                state = torch.Tensor(prepro(env.reset()))
+                state = torch.tensor(prepro(env.reset()))
 
             values.append(value) ; logps.append(logp) ; actions.append(action) ; rewards.append(reward)
 
-        next_value = Variable(torch.zeros(1,1)) if done else model((Variable(state.unsqueeze(0)), (hx, cx)))[0]
-        values.append(Variable(next_value.data))
+        next_value = torch.zeros(1,1) if done else model((state.unsqueeze(0), hx))[0]
+        values.append(next_value.detach())
 
-        loss = cost_func(torch.cat(values), torch.cat(logps), torch.cat(actions), np.asarray(rewards))
-        eploss += loss.data[0]
+        loss = cost_func(args, torch.cat(values), torch.cat(logps), torch.cat(actions), np.asarray(rewards))
+        eploss += loss.item()
         shared_optimizer.zero_grad() ; loss.backward()
-        torch.nn.utils.clip_grad_norm(model.parameters(), 40)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 40)
 
         for param, shared_param in zip(model.parameters(), shared_model.parameters()):
             if shared_param.grad is None: shared_param._grad = param.grad # sync gradients with shared model
         shared_optimizer.step()
 
-def cost_func(values, logps, actions, rewards):
-    np_values = values.view(-1).data.numpy()
-
-    # generalized advantage estimation (a policy gradient method)
-    delta_t = np.asarray(rewards) + args.gamma * np_values[1:] - np_values[:-1]
-    gae = discount(delta_t, args.gamma * args.tau)
-    logpys = logps.gather(1, Variable(actions).view(-1,1))
-    policy_loss = -(logpys.view(-1) * Variable(torch.Tensor(gae))).sum()
+if __name__ == "__main__":
+    if sys.version_info[0] > 2:
+        mp.set_start_method('spawn') # this must not be in global scope
+    elif sys.platform == 'linux' or sys.platform == 'linux2':
+        raise "Must be using Python 3 with linux!" # or else you get a deadlock in conv2d
     
-    # l2 loss over value estimator
-    rewards[-1] += args.gamma * np_values[-1]
-    discounted_r = discount(np.asarray(rewards), args.gamma)
-    discounted_r = Variable(torch.Tensor(discounted_r))
-    value_loss = .5 * (discounted_r - values[:-1,0]).pow(2).sum()
+    args = get_args()
+    args.save_dir = '{}/'.format(args.env.lower()) # keep the directory structure simple
+    if args.render:  args.processes = 1 ; args.test = True # render mode -> test mode w one process
+    if args.test:  args.lr = 0 # don't train in render mode
+    args.num_actions = gym.make(args.env).action_space.n # get the action space of this game
+    os.makedirs(args.save_dir) if not os.path.exists(args.save_dir) else None # make dir to save models etc.
 
-    entropy_loss = -(-logps * torch.exp(logps)).sum() # encourage lower entropy
-    return policy_loss + 0.5 * value_loss + 0.01 * entropy_loss
+    torch.manual_seed(args.seed)
+    shared_model = NNPolicy(channels=1, memsize=args.hidden, num_actions=args.num_actions).share_memory()
+    shared_optimizer = SharedAdam(shared_model.parameters(), lr=args.lr)
 
-processes = []
-for rank in range(args.processes):
-    p = mp.Process(target=train, args=(rank, args, info))
-    p.start() ; processes.append(p)
-for p in processes:
-    p.join()
+    info = {k: torch.DoubleTensor([0]).share_memory_() for k in ['run_epr', 'run_loss', 'episodes', 'frames']}
+    info['frames'] += shared_model.try_load(args.save_dir) * 1e6
+    if int(info['frames'].item()) == 0: printlog(args,'', end='', mode='w') # clear log file
+    
+    processes = []
+    for rank in range(args.processes):
+        p = mp.Process(target=train, args=(shared_model, shared_optimizer, rank, args, info))
+        p.start() ; processes.append(p)
+    for p in processes: p.join()
