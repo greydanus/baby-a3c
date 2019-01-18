85899d76bd1a72d9e9055ed35894390802e31240
Sam <greydanus@users.noreply.github.com>
2019-01-18 14:04:18 -0800

    Fix entropy regularization to be more readable

--- baby-a3c.py
+++ baby-a3c.py
@@ -92,8 +92,8 @@ def cost_func(args, values, logps, actions, rewards):
     discounted_r = torch.tensor(discounted_r.copy(), dtype=torch.float32)
     value_loss = .5 * (discounted_r - values[:-1,0]).pow(2).sum()
 
-    entropy_loss = (-logps * torch.exp(logps)).sum() # encourage lower entropy
-    return policy_loss + 0.5 * value_loss + 0.01 * entropy_loss
+    entropy_loss = (-logps * torch.exp(logps)).sum() # entropy definition, for entropy regularization
+    return policy_loss + 0.5 * value_loss - 0.01 * entropy_loss
 
 def train(shared_model, shared_optimizer, rank, args, info):
     env = gym.make(args.env) # make a local (unshared) environment
